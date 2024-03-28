46062af4547a8183d7030d617838c39046b0ddff
Caio de Araújo <caiotdearaujo@gmail.com>
2024-03-28 13:52:45 -0300

    ex6 added

---
+++ ex6/index.js
@@ -0,0 +1,10 @@
+const nome = "Caio";
+const sobrenome = "de Araújo";
+const idade = 16;
+const pesoEmKg = 60;
+const alturaEmM = 1.75;
+const imc = peso / altura ** 2;
+const anoDeNascimento = 2007;
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}kg, tem ${altura}m de altura e seu IMC é de ${imc}kg/m²`);
+console.log(`${nome} nasceu em ${anoDeNascimento}`);
