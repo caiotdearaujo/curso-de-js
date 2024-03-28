5b753a1f46c5e4f80d6cef4deb10c2e36ebe6261
Caio de Araújo <caiotdearaujo@gmail.com>
2024-03-27 21:37:19 -0300

    ex4 added

---
+++ ex4/index.js
@@ -0,0 +1,16 @@
+let nome; // declaração
+nome = "João"; // inicialização
+
+console.log(nome);
+
+nome = "Caio"; // reatribuição de valor
+
+console.log(nome)
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não podem iniciar com números nem conter espaços ou hífens
+// Utiliza-se camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilizar var, mas sim let
