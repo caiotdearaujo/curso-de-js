380f4e024b60bae55351cbb316a2011de3c8056b
Caio de Araújo <caiotdearaujo@gmail.com>
2024-03-27 21:43:20 -0300

    ex5 added

---
+++ ex5/index.js
@@ -0,0 +1,14 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não podem iniciar com números nem conter espaços ou hífens
+// Utiliza-se camelCase
+// Case-sensitive
+// Não podemos modificar o valor de uma constante
+
+const nome = "João";
+const idade = 21;
+console.log(nome, "tem", idade, "anos");
+
+// Tipos de dados básicos
+// String = texto
+// Number = número
