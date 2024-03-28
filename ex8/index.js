a982237b74cb6d0e0f369a1499de72ec0ea18387
Caio de Araújo <caiotdearaujo@gmail.com>
2024-03-28 14:50:00 -0300

    ex8 added

---
+++ ex8/index.js
@@ -0,0 +1,30 @@
+/**
+ * OPERADORES
+ * 
+ * ARITMÉTICOS
+ * + soma/concatenação
+ * - subtração
+ * * multiplicação
+ * / divisão
+ * ** potenciação
+ * % módulo da divisão
+ * 
+ * ATRIBUIÇÃO
+ * = atribui valor à variável
+ * 
+ * INCREMENTO (i = i+1)
+ * i++ (operação ocorre após/pós-incremento) 
+ * ++i (operação ocorre primeiro/pré-incremento)
+ * 
+ * DECREMENTO (i = i-1)
+ * i-- (operação ocorre após/pós-decremento)
+ * --i (operação ocorre primeiro/pré-decremento)
+ * 
+ * ATRIBUIÇÃO COM OPERAÇÃO
+ * x += y // x = x+y
+ * x -= y // x = x-y
+ * x *= y // x = x*y
+ * x /= y // x = x/y
+ * x **= y // x = x**y
+ * x %= y // x = x%y
+ */
